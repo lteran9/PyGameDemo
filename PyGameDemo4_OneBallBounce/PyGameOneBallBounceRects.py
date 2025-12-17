19d13c56b3421bc52a390a734b8b7bc1ff6e96be
LT <support@teran.tech>
2025-12-17 08:37:02 -0700

    PyGame Demo 4 - One Ball Bounce - Created PyGameOneBallBounceRects.py which uses a Rect object to calculate position of image

--- PyGameDemo4_OneBallBounce/PyGameOneBallBounceRects.py
+++ PyGameDemo4_OneBallBounce/PyGameOneBallBounceRects.py
@@ -1,4 +1,4 @@
-# pygame demo 4(a), one image, bounce around the window using (x, y) coords
+# pygame demo 4(b), one image, bounce around the window using rects
 
 # 1 - Import packages
 import pygame
@@ -10,9 +10,8 @@ import random
 BLACK = (0, 0, 0)
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
-FRAMES_PER_SECOND = 30
-BALL_WIDTH_HEIGHT = 100
-N_PIXELS_PER_FRAME = 3
+FRAMES_PER_SECOND = 60
+N_PIXELS_PER_FRAME = 12
 
 # 3 - Initialize the world
 pygame.init()
@@ -23,10 +22,11 @@ clock = pygame.time.Clock()
 ballImage = pygame.image.load('images/ball.png')
 
 # 5 - Initialize variables
-MAX_WIDTH = WINDOW_WIDTH - BALL_WIDTH_HEIGHT
-MAX_HEIGHT = WINDOW_HEIGHT - BALL_WIDTH_HEIGHT
-ballX = random.randrange(MAX_WIDTH)
-ballY = random.randrange(MAX_HEIGHT)
+ballRect = ballImage.get_rect()
+MAX_WIDTH = WINDOW_WIDTH - ballRect.width
+MAX_HEIGHT = WINDOW_HEIGHT - ballRect.height
+ballRect.left = random.randrange(MAX_WIDTH)
+ballRect.top = random.randrange(MAX_HEIGHT)
 xSpeed = N_PIXELS_PER_FRAME
 ySpeed = N_PIXELS_PER_FRAME
 
@@ -40,25 +40,26 @@ while True:
             pygame.quit()
             sys.exit()
 
+    
     # 8 - Do any "per frame" actions
-    if ballX < 0 or ballX >= MAX_WIDTH:
+    if ballRect.left < 0 or ballRect.left >= MAX_WIDTH:
         xSpeed = -xSpeed # reverse x direction
 
-    if ballY < 0 or ballY >= MAX_HEIGHT:
+    if ballRect.top < 0 or ballRect.top >= MAX_HEIGHT:
         ySpeed = -ySpeed # reverse y direction
 
-    # Update the ball's location, using the speed in two directions
-    ballX = ballX + xSpeed
-    ballY = ballY + ySpeed
+    # Update the ball's rectangle using the speed in two directions
+    ballRect.left = ballRect.left + xSpeed
+    ballRect.top = ballRect.top + ySpeed
 
     # 9 - Clear the window before drawing it again
     window.fill(BLACK)
 
     # 10 - Draw the window elements
-    window.blit(ballImage, (ballX, ballY))
+    window.blit(ballImage, ballRect)
 
     # 11 - Update the window
     pygame.display.update()
 
     # 12 - Slow things down a bit
-    clock.tick(FRAMES_PER_SECOND)
+    clock.tick(N_PIXELS_PER_FRAME)
